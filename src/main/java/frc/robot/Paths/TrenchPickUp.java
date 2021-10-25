8ee77f44e3434ad43aab557944125686e67e7faa
Programming 2 <apanteli23@team3467.org>
2021-10-25 19:21:09 -0400

    Untested code for autonomous trench pickups

--- src/main/java/frc/robot/Paths/TrenchPickUp.java
+++ src/main/java/frc/robot/Paths/TrenchPickUp.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot;
+package frc.robot.Paths;
 
 import java.io.IOException;
 import java.nio.file.Path;
@@ -13,69 +13,19 @@ import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj.controller.RamseteController;
 import edu.wpi.first.wpilibj.trajectory.Trajectory;
 import edu.wpi.first.wpilibj.trajectory.TrajectoryConfig;
-import edu.wpi.first.wpilibj.trajectory.TrajectoryUtil;
 import edu.wpi.first.wpilibj.trajectory.constraint.DifferentialDriveVoltageConstraint;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RamseteCommand;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryUtil;
+
+
 import frc.robot.Constants.DriveConstants;
-import frc.robot.Constants.OIConstants;
-import frc.robot.IMU.IMU;
-import frc.robot.IMU.IMU_ReportStatus;
 import frc.robot.Subsystems.DriveSubsystem.DriveSubsystem;
-import frc.robot.Subsystems.DriveSubsystem.SplitArcadeDrive;
-import frc.robot.control.XboxController;
-/**
- * This class is where the bulk of the robot should be declared. Since
- * Command-based is a "declarative" paradigm, very little robot logic should
- * actually be handled in the {@link Robot} periodic methods (other than the
- * scheduler calls). Instead, the structure of the robot (including subsystems,
- * commands, and button mappings) should be declared here.
- */
-public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
 
+public class TrenchPickUp{
   private final DriveSubsystem m_robotDrive = new DriveSubsystem();
-  
-  public static XboxController m_driverController = new XboxController(OIConstants.kDriverControllerPort);
-  public static XboxController m_operatorController = new XboxController(OIConstants.kOperatorControllerPort);
-  
-  /**
-   * The container for the robot. Contains subsystems, OI devices, and commands.
-   */
-  public RobotContainer() {
-    // Configure the button bindings
-    configureButtonBindings();
 
-    m_robotDrive.setDefaultCommand(
-
-        //Driver Controller 
-        //Split Arcade: forward/back leftY, right/left rightX
-        new SplitArcadeDrive(m_robotDrive, 
-                            () -> m_driverController.getLeftY(),
-                            () -> m_driverController.getRightX()));  
-
-    
-    IMU gyro = IMU.getInstance();
-    
-    new IMU_ReportStatus(gyro);
-  }
-
-  /**
-   * Use this method to define your button->command mappings. Buttons can be created by
-   * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
-   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-   */
-  private void configureButtonBindings() {}
-
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
-  public Command getAutonomousCommand() {
-    //Leaving this here for testing purposes - I am going to replace with Trench Pickup 
-
+  public Command TrenchPickUp() {  
     // Create a voltage constraint to ensure we don't accelerate too fast
     var autoVoltageConstraint =
         new DifferentialDriveVoltageConstraint(DriveConstants.kVoltageFeedForward,
@@ -91,8 +41,7 @@ public class RobotContainer {
             // Apply the voltage constraint
             .addConstraint(autoVoltageConstraint);
 
-    // An example trajectory to follow.  All units in meters.
-    
+  
     Trajectory trenchPickUp = new Trajectory();
     String trajectoryJson = "Paths/TrenchPickup.wpilib.json";
     try{
